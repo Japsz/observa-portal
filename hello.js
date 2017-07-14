036f9d775d0c6bfe8cad8dc8538167d66fde4fa6
Japsz <benjamin.meneses.14@sansano.usm.cl>
2017-07-14 14:13:24 -0400

    ups
    la clave

--- hello.js
+++ hello.js
@@ -38,7 +38,7 @@ app.use(
 
         host: '127.0.0.1',
         user: 'root',
-        password : '1234',
+        password : 'observaproyecta',
         port : 3306,
         database:'Observapp'
 
